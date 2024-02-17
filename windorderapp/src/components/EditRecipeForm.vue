32580b5a0f1810aee5fcf217a95347146ce021a6
marco-caplena <marco@caplena.com>
2024-02-17 23:14:48 +0100

    fix: thick title in edit mode

--- windorderapp/src/components/EditRecipeForm.vue
+++ windorderapp/src/components/EditRecipeForm.vue
@@ -10,22 +10,58 @@ const selectOptions = [
     value: 'secondi_di_carne',
   },
   {
+    label: 'Torte Salate',
+    value: 'torte_salate'
+  },
+  {
+    label: 'Dolci',
+    value: 'dolci'
+  },
+  {
+    label: 'Pane & Co.',
+    value: 'pane_&_co'
+  },
+  {
+    label: 'Sughi',
+    value: 'sughi'
+  },
+  {
+    label: "Salse",
+    value: 'salse',
+  },
+  {
     label: 'Secondi di Pesce',
     value: 'secondi_di_pesce'
+  },
+  {
+    label: 'Verdure',
+    value: 'verdure'
+  },
+  {
+    label: 'Creme Dolci',
+    value: 'creme_dolci'
+  },
+  {
+    label: 'Paste Base',
+    value: 'paste_base'
+  },
+  {
+    label: 'Antipasti',
+    value: 'antipasti'
   }
 ]
 </script>
 
 <template>
     <n-space vertical style="padding: 10px 20px 10px 20px">
-          <n-input class="txt_box" v-model:value="edited_recipe.title"/>
+          <n-input style="font-size: 30px" v-model:value="edited_recipe.title" placeholder="Basic Input" />
           <n-select v-model:value="edited_recipe.category" :options="selectOptions"/>
           <n-thing title="Ingredienti"/>
-          <n-input v-model:value="edited_recipe.ingredients" type="textarea" placeholder="Autosizable" :autosize="{minRows: 3}"/>
+          <n-input v-model:value="edited_recipe.ingredients" type="textarea" placeholder="Ingredienti" :autosize="{minRows: 3}"/>
           <n-thing title="Ricetta"/>
-          <n-input v-model:value="edited_recipe.steps" type="textarea" placeholder="Autosizable" :autosize="{minRows: 3}"/>
+          <n-input v-model:value="edited_recipe.steps" type="textarea" placeholder="Ricetta" :autosize="{minRows: 3}"/>
           <n-thing title="Note"/>
-          <n-input v-model:value="edited_recipe.notes" type="textarea" placeholder="Autosizable" :autosize="{minRows: 3}"/>
+          <n-input v-model:value="edited_recipe.notes" type="textarea" placeholder="Note" :autosize="{minRows: 3}"/>
         <n-space>
           <n-button type="primary" @click="$emit('SaveRecipe', edited_recipe)">
           Save
@@ -36,9 +72,3 @@ const selectOptions = [
         </n-space>
       </n-space>
   </template>
-<style>
-.txt_box {
-  font-size: 30px;   
-  height: fit-content;
-}
-</style> 
